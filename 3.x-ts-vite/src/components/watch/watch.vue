ecae836f329d992bc1c41ff0472df2dc13e713d3
YonSunZhen <1315976441@qq.com>
2022-10-08 22:14:39 +0800

    update

---
+++ 3.x-ts-vite/src/components/watch/watch.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Author: yongzhen.sun
+ * @Date: 2022-09-22 08:47:05
+ * @LastEditors: yongzhen.sun
+ * @LastEditTime: 2022-09-22 09:08:40
+ * @Description: file content
+-->
+<template>
+{{test1.name}}
+</template>
+
+<script lang="ts" setup name="Computed">
+import { ref, defineComponent, computed } from 'vue'
+const props = defineProps({
+  test: {
+    type: Object,
+    default: () => ({})
+  }
+})
+const test1 = computed(() => {
+  console.log('debug1')
+  return props.test
+})
+// const test1 = ref(props.test)
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
+  
