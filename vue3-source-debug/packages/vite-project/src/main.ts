2cb200ad440dc8b3ff940e01a91b6b2c02d673ea
yongzhen.sun <1315976441@qq.com>
2022-11-16 10:54:46 +0800

    更新 vue3-source-debug

---
+++ vue3-source-debug/packages/vite-project/src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: yongzhen.sun
+ * @Date: 2022-11-16 09:19:40
+ * @LastEditors: yongzhen.sun
+ * @LastEditTime: 2022-11-16 09:34:24
+ * @Description: file content
+ */
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+debugger
+createApp(App).mount('#app')
