c193b16a3a55fe1d4535b86c63a0b126bf0773b7
yongzhen.sun <1315976441@qq.com>
2022-12-27 11:14:23 +0800

    补充提交

---
+++ 3.x-ts-vite/src/components/watch/Watch.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: yongzhen.sun
+ * @Date: 2022-09-22 08:47:05
+ * @LastEditors: yongzhen.sun
+ * @LastEditTime: 2022-12-27 11:14:15
+ * @Description: file content
+-->
+<template>
+  <button @click="clickRef">ref11</button>
+  <button @click="clickReactive">reactive11</button>
+  </template>
+  
+  <script lang="ts" setup name="WatchDemo">
+  import { ref, reactive, watch } from 'vue'
+  const testRef = ref({
+    name: 'aaa'
+  })
+  const testReactive = reactive({
+    name: 'bbb'
+  })
+  // 监听 ref 数据
+  watch(testRef, (val) => {
+    console.log('ref改变了');
+  }, { deep: true})
+  // // 监听 reactive 数据
+  // watch(testReactive, (val) => {
+  //   console.log('reactive改变了');
+  // })
+  // 监听多个值
+  watch([testRef, testReactive],  () => {
+    console.log('改变了');
+  })
+  
+  const clickRef = () => {
+    console.log('debug1')
+    testRef.value.name = 'aaa1'
+  }
+  const clickReactive = () => {
+    testReactive.name = 'bbb1'
+  }
+  
+  </script>
+  
+  <style scoped>
+  </style>
+    
